cc6701cbe272ae8dd5c33602ea2df962f1cb8e37
BY <alucard_118@163.com>
2017-05-05 22:44:23 +0800

    5-5-3

--- routes/login.js
+++ routes/login.js
@@ -1,19 +1,18 @@
 var express=require('express');
 var router=express.Router();
-var ccap=require('ccap');
+var code=require('../lib/captcha.js');
 var fs=require('fs');
 
 
 
 router.get('/',function (req,res) {
-	var captcha=ccap();
-	var txt=captcha.get()[0];
-	var buf=captcha.get()[1];
-	fs.writeFile('./public/images/imgcode.jpg',buf,function (err) {
-		if(err) console.log(err);
-	})
+	var txt=code.codeController(4);
 	console.log(txt);
 	res.render('login');
+})
+
+router.get('/reborncode',function (req,res) {
+	
 })
 
 
