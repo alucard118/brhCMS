28ca1ed51f12891c96b62b63213f6967ed68ded3
BY <by@BYdeiMac.local>
2017-05-05 17:42:29 +0800

    5-2-2

--- public/js/login.js
+++ public/js/login.js
@@ -3,4 +3,10 @@ $(document).ready(function () {
 	var top=($(window).height()-$('.login').outerHeight())/3;
 	$('.login').css({'left':left,'top':top});
 
+	$(window).resize(function () {
+		var left=($(window).width()-$('.login').outerWidth())/2;
+		var top=($(window).height()-$('.login').outerHeight())/3;
+		$('.login').css({'left':left,'top':top});
+	});
+
 });
