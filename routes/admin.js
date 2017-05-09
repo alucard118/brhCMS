11fd11e67e42fd37a58b900195d2ae29c6b8c56c
BY <alucard_118@163.com>
2017-05-09 22:12:31 +0800

    5-9-3

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,36 @@
 var express=require('express');
 var router=express.Router();
-
+var title="CNCC2017";
 
 router.get('/',function (req,res) {
+	res.redirect('/admin/home');
+})
+
+router.get('/home',function (req,res) {
 	if(req.session.role=='superAdmin'){
-		res.render('admin');
+		res.render('./admin/admin_home',{siteTitle:title,user:req.session.user});
 	}
 	else if(req.session.user===undefined && req.session.role===undefined){
 		res.render('noprevelige');
 	}
 	
+});
+router.get('/news',function (req,res) {
+	if(req.session.role=='superAdmin'){
+		res.render('./admin/admin_news',{siteTitle:title,user:req.session.user});
+	}
+	else if(req.session.user===undefined && req.session.role===undefined){
+		res.render('./admin/noprevelige');
+	}
+	
+});
+
+router.get('/logout',function (req,res) {
+	req.session.destroy(function (err) {
+		res.redirect('/login');
+	})
+	
 })
+
 
 module.exports=router;
