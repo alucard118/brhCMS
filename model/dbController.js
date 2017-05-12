956314ab7c72108529d6ee714a7a91b7ca3beb3e
BY <by@BYdeiMac.local>
2017-05-12 12:00:46 +0800

    5-12-1

--- model/dbController.js
+++ model/dbController.js
@@ -5,7 +5,7 @@ var url="mongodb://brhcms:ccfinfo2@localhost:27017/brhCms";
 
 var dbController={
 	checkUser:function (username,password,callback) {
-		MongoClient.connect(url,function (err,db) {
+		MongoClient.connect(url,{auto_reconnect:true},function (err,db) {
 			db.collection('brhCms_users',function (err,collection) {
 
 				collection.find({'name':username,'password':password}).toArray(function (err,docs) {
