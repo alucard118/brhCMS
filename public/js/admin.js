8de03725793eb9c801c915fe7c55c8d4b45780e5
BY <alucard_118@163.com>
2017-05-13 14:25:26 +0800

    5-13-2

--- public/js/admin.js
+++ public/js/admin.js
@@ -2,4 +2,16 @@ $(document).ready(function () {
 	$('#createNews').click(function () {
 		location.href="/admin/createNews";
 	});
+
+	$('#setTop').bind('click',function () {
+		if($('#setTop').is(':checked')){
+			
+			$('#endTime').fadeIn('fast');
+		}
+		else{
+			$('#endTime').fadeOut('fast');
+			$('#endTime').val('');
+		}
+	})
+		
 });
